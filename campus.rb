f98d418ff54527a49be4e532594178c37310c22d
Dairaku <dairaku051018@gmail.com>
2015-08-26 14:49:06 +0900

    中間課題直しました。

--- campus.rb
+++ campus.rb
@@ -8,30 +8,45 @@ class Pokemon
 	end
 end
 
-Pika = Pokemon.new("ピカチュウ",30,13)
-Zeni = Pokemon.new("ゼニガメ",50,6)
+Pika = Pokemon.new("ピカチュウ",30,5)
+Zeni = Pokemon.new("ゼニガメ",50,8)
 
 puts "*************************","戦闘開始","*************************"
 
+	if Pika.attack > Zeni.attack then
 
-1.upto(5) do |i|
+		1.upto(5) do |i|
+			puts "-----","第#{i}戦","-----"
 
-	puts "-----","第#{i}戦","-----"
+			Zeni.hp = Zeni.hp - Pika.attack
 
-	puts "ピカチュウの残りのHP:#{Pika.hp}"
+			puts "ピカチュウの残りのHP:#{Pika.hp}"
+			puts "ゼニガメの残りのHP:#{Zeni.hp}"
 
-	Zeni.hp = Zeni.hp - Pika.attack
-	puts "ゼニガメの残りのHP:#{Zeni.hp}"
+			if Zeni.hp < 0
+				sleep 2
+				puts "*************************","決着!","*************************","ゼニガメ瀕死!ピカチュウの勝利！" 
+				break #=> uptoを抜ける
+			end
+			sleep 1 #=> 1秒待つ
+		end
 
-	if Zeni.hp < 0
-		sleep 2
-		puts "*************************","決着!","*************************","ゼニガメ瀕死!ピカチュウの勝利！" 
-		break #=> uptoを抜ける
+	else
+		1.upto(5) do |i|
+			puts "-----","第#{i}戦","-----"
+
+			Pika.hp = Pika.hp - Zeni.attack
+			puts "ピカチュウの残りのHP:#{Pika.hp}"
+			puts "ゼニガメの残りのHP:#{Zeni.hp}"
+
+			if Pika.hp < 0
+				sleep 2
+				puts "*************************","決着!","*************************","ピカチュウ瀕死!ゼニガメの勝利！" 
+				break #=> uptoを抜ける
+			end
+			sleep 1 #=> 1秒待つ
+		end
 	end
-
-	sleep 1 #=> 1秒待つ
-
-end
 
 
 
